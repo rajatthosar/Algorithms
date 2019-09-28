ea35e64628bff079abd4b95442b15a81256c7162
Rajat Thosar <rajat.thosar@yahoo.in>
2019-09-28 04:19:49 -0400

    code cleanup

--- HackerRank/GS/running_avg.py
+++
@@ -1,9 +0,0 @@
-data = [1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7, 7]
-
-def whatever():
-    running_avg_list = []
-    for index in range(6, len(data)):
-        avg = sum(data[index - 6:index + 1]) / 7
-        running_avg_list.append(str(round(avg, 2)))
-
-    return running_avg_list
